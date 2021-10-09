28057f84175e60b623a15e3d6f52f6f78fa8a0d5
nh-polash <polashhoque0@gmail.com>
2021-10-09 16:54:29 +0600

    added some css

--- 33.4-mealDB/script.js
+++ 33.4-mealDB/script.js
@@ -14,13 +14,13 @@ const displaySearchResult = meals => {
     meals.forEach(meal => {
         console.log(meal)
         const div = document.createElement('div');
-        div.classList.add('col');
+        div.classList.add('col-md-3');
         div.innerHTML = `
-        <div class="card">
+        <div class="card shadow">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${meal.strMeal}</h5>
-                <p class="card-text">${meal.strInstructions.slice(0, 200)}.</p>
+                <h3 class="card-title">${meal.strMeal}</h3>
+                <p class="card-text">${meal.strInstructions.slice(0, 160)}</p>
             </div>
         </div>
         `;
