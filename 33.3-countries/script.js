29a825f74c7c2683b19d2acd21c542310dcfe3be
nh-polash <polashhoque0@gmail.com>
2021-10-09 13:22:06 +0600

    modified website layout

--- 33.3-countries/script.js
+++ 33.3-countries/script.js
@@ -1,5 +1,5 @@
 const loadCountries = () => {
-    fetch('https://restcountries.com/v3.1/all')
+    fetch('https://restcountries.com/v2/all')
     .then(res => res.json())
     .then(data => displayCountries(data))
 }
@@ -11,13 +11,38 @@ const displayCountries = countries => {
         
     // }
     countries.forEach(country => {
-        console.log(country)
+        // console.log(country)
         const div = document.createElement('div');
         div.innerHTML = `
-            <img src="${country.flags.png}">
-            <h1>${country.name.common}</h1>
-            <h1>${country.capital}</h1>
+            <h2>${country.name}</h2>
+            <h5>Capital: ${country.capital}</h5>
+            <button onclick="loadCountryByName('${country.name}')">
+                Details
+            </button>
         `
         countriesDiv.appendChild(div);
     });
+}
+
+
+const loadCountryByName = name => {
+    // console.log(name)
+    const url = `https://restcountries.com/v2/name/${name}`;
+    console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetails(data));
+}
+
+const displayCountryDetails = country => {
+    const countryNew = country[0];
+    const card = document.getElementById('card');
+    card.style.display = 'block';
+    card.innerHTML = `
+        <img src="${countryNew.flag}">
+        <h1>Name: ${countryNew.name}</h1>
+        <h4>Capital: ${countryNew.name}</h4>
+        <p>Population: ${countryNew.population}</p>
+        <p>Currency: ${countryNew.currencies[0].name}</p>
+    `
 }
